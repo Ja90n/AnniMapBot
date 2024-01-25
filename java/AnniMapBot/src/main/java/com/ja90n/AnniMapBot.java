3ef8f30f3aca3a70c1798c294cb8c8876ec62f2d
Ja90n <jadonvanbeilen@gmail.com>
2024-01-25 01:40:25 +0100

    it now works i hope

--- java/AnniMapBot/src/main/java/com/ja90n/AnniMapBot.java
+++ java/AnniMapBot/src/main/java/com/ja90n/AnniMapBot.java
@@ -6,6 +6,8 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Message;
 
+import java.util.Date;
+
 import static net.dv8tion.jda.api.requests.GatewayIntent.DIRECT_MESSAGES;
 import static net.dv8tion.jda.api.requests.GatewayIntent.GUILD_MESSAGES;
 
@@ -26,7 +28,25 @@ public class AnniMapBot {
 
     public void gotReply(String reply) {
         System.out.println("Got reply");
+
+        StringBuilder replyEditor = new StringBuilder(reply);
+        replyEditor.delete(0,1);
+
+        String message = "";
+        StringBuilder stringBuilder = new StringBuilder(message);
+        String[] list = replyEditor.toString().split("!");
+        for (String map : list) {
+            System.out.println(map);
+            String[] part = map.split(",");
+            stringBuilder.append(part[0]).append(", ").append(part[1]).append(", ").append(part[2]).append("\n");
+        }
+
         Message loadingMessage = bot.getTextChannelById(1199795392615829585L).retrieveMessageById(1199842439163293729L).complete();
-        loadingMessage.editMessage(reply).complete();
+        stringBuilder.append("Last update: <t:").append(Math.round((float) new Date().getTime() /1000)).append(":R>");
+        loadingMessage.editMessage(stringBuilder).complete();
+    }
+
+    public JDA getBot() {
+        return bot;
     }
 }
--- java/AnniMapBot/src/main/java/com/ja90n/events/ReactionEvent.java
+++ java/AnniMapBot/src/main/java/com/ja90n/events/ReactionEvent.java
@@ -9,6 +9,8 @@ import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import java.net.URI;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ReactionEvent extends ListenerAdapter {
 
@@ -23,18 +25,26 @@ public class ReactionEvent extends ListenerAdapter {
         if (event.getUser().isBot()) return;
         if (!event.getMessageId().equals("1199831871471751268")) return;
 
+        event.getReaction().removeReaction(anniMapBot.getBot().getSelfUser()).queue();
+        event.getReaction().removeReaction(event.getUser()).queue();
+
         if (event.getReaction().getEmoji().getAsReactionCode().equals("✅")) {
             try {
                 Client c = new Client(new URI("ws://localhost:7080"), anniMapBot);
                 c.connect();
                 c.close();
             } catch (Exception e){}
+            Message loadingMessage = anniMapBot.getBot().getTextChannelById(1199795392615829585L).retrieveMessageById(1199842439163293729L).complete();
+            loadingMessage.editMessage("Loading...").queue();
         }
 
-        event.getReaction().removeReaction(event.getUser()).queue();
-        //event.getReaction().removeReaction(bot.getSelfUser()).queue();
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            public void run() {
+                anniMapBot.getBot().getTextChannelById(1199795392615829585L).retrieveMessageById(1199831871471751268L).complete().addReaction(Emoji.fromUnicode("U+2705")).queue();
+            }
+        };
 
-        Message message = event.retrieveMessage().complete();
-        message.addReaction(Emoji.fromUnicode("U+2705")).queue();
+        timer.schedule(task,5000);
     }
 }
